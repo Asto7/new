eb701ef5f50381241946a8cb4d31b7f26ef21319
Asto7 <ssatyanand7@gmail.com>
2019-07-12 20:15:54 +0530

    change

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express=require('express');
-
+var port=process.env.PORT||9999;
 var app=express();
 
 app.use(express.static('./public'));
 app.get('/',function(req,res){
   res.redirect('./public/index.html');
 })
-app.listen(9999);
+app.listen(port);
